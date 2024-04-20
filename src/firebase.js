408a1291377a0e4f16035bc22f8646cbcc5abe53
suryanarayanan <110848003+shakthisurya@users.noreply.github.com>
2024-04-20 14:00:36 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDHTYLvobv0PsAq_aDeuwRY4btPGFBc5GE",
-    authDomain: "todo-aead4.firebaseapp.com",
-    databaseURL: "https://todo-aead4-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "todo-aead4",
-    storageBucket: "todo-aead4.appspot.com",
-    messagingSenderId: "921035371335",
-    appId: "1:921035371335:web:1077c314de4651587ad262",
-    measurementId: "G-39CZ6DG98L"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
